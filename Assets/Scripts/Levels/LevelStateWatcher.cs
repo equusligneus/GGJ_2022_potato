4a2783dcf41c14284e7ea4cc05c28a282be38067
Frederik Reher <reher.frederik@gmail.com>
2022-01-22 00:32:33 +0100

    Added change checks to LevelStateWatcher

--- Assets/Scripts/Levels/LevelStateWatcher.cs
+++ Assets/Scripts/Levels/LevelStateWatcher.cs
@@ -12,5 +12,37 @@ public class LevelStateWatcher : MonoBehaviour
 
 	public World CurrentWorld
 		=> (IsInLevel && currentWorld != default) ? currentWorld.Value : World.Invalid;
- 
+
+
+	public void OnStart()
+	{
+		if(currentLevel != default)
+			currentLevel.OnValueChanged += CurrentLevel_OnValueChanged;
+
+		if(currentWorld != default)
+			currentWorld.OnValueChanged += CurrentWorld_OnValueChanged;
+	}
+
+	public void OnDestroy()
+	{
+		if (currentLevel != default)
+			currentLevel.OnValueChanged -= CurrentLevel_OnValueChanged;
+
+		if (currentWorld != default)
+			currentWorld.OnValueChanged -= CurrentWorld_OnValueChanged;
+	}
+
+	private void CurrentWorld_OnValueChanged(World _value)
+		=> SetAnimValues(CurrentWorld);
+
+	private void CurrentLevel_OnValueChanged(Level _value)
+		=> SetAnimValues(CurrentWorld);
+
+	private void SetAnimValues(World _value)
+	{
+		if (_value == World.Invalid)
+			return;
+
+		// do animator stuff here!!!
+	}
 }
