551dbdeb12fbe0a63e316d6f848ef3a4bf585294
b34s7m0d3 <peter@petermehlmann.com>
2022-01-21 23:09:33 +0100

    Animations. Also changed something with levelStateWatcher pls no hurt

--- Assets/Scripts/Levels/LevelStateWatcher.cs
+++ Assets/Scripts/Levels/LevelStateWatcher.cs
@@ -5,10 +5,12 @@ public class LevelStateWatcher : MonoBehaviour
 	[SerializeField] private Level myLevel;
 	[SerializeField] private RuntimeLevel currentLevel;
 	[SerializeField] private RuntimeWorld currentWorld;
+	[SerializeField] private Animator myAnimator;
 
 	public bool IsInLevel
 		=> currentLevel != default && currentLevel.Equals(myLevel);
 
 	public World CurrentWorld
 		=> (IsInLevel && currentWorld != default) ? currentWorld.Value : World.Invalid;
+ 
 }
